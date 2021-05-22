71610f1f5f2cb6e4f7ecd930466f8f44aad16049
christopherrobin <christopherrobinreynolds@gmail.com>
2021-05-22 16:21:19 -0400

    Switching up the POST format

--- src/CoinbaseAuth.js
+++ src/CoinbaseAuth.js
@@ -9,18 +9,31 @@ const CoinbaseAuth = (props) => {
     const connectToCoinbase = () => window.location.replace('https://www.coinbase.com/oauth/authorize?client_id=b15a8f09ba059b65e41be40e61f0fa4ccf64d965538d886e11d0946eb59a17d1&redirect_uri=https%3A%2F%2Fwww.cryptalyzer.com%2Fhello&response_type=code&scope=wallet%3Auser%3Aread');
 
 useEffect(() => {
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: `grant_type=authorization_code&code=${coinbaseCode}&client_id=b15a8f09ba059b65e41be40e61f0fa4ccf64d965538d886e11d0946eb59a17d1&client_secret=f6d6e442eea4960d0b236644ac474d0e9e7cecc3d984b31268eca1ba9696b027&redirect_uri=https://www.cryptalyzer.com/hello&enablePKCE=false&scopes=["wallet:user:read"]`
-    };
-    if(coinbaseCode) {
-    fetch('https://api.coinbase.com/oauth/token', requestOptions)
-        .then(response => response.json())
-        .then(data => console.log(data));
-    }
 
-// empty dependency array means this effect will only run once (like componentDidMount in classes)
+var myHeaders = new Headers();
+myHeaders.append("Cookie", "__cf_bm=129606ad9687effb18287759964a9262e3e22da3-1621714317-1800-AUImc3ODIsnDvoEDgRsJhOcSwHg7zjPLn+hDySV48raVjzFJlFZssuF4iWPvTsbNUAhSr7n/mEySfz+VGOOpQJU=; amplitude_device_id=da721d2c-e2ae-4b60-8b9e-e9d4ea2adec6; coinbase_device_id=da721d2c-e2ae-4b60-8b9e-e9d4ea2adec6");
+
+var formdata = new FormData();
+formdata.append("grant_type", "authorization_code");
+formdata.append("code", coinbaseCode);
+formdata.append("client_id", "b15a8f09ba059b65e41be40e61f0fa4ccf64d965538d886e11d0946eb59a17d1");
+formdata.append("client_secret", "f6d6e442eea4960d0b236644ac474d0e9e7cecc3d984b31268eca1ba9696b027");
+formdata.append("redirect_uri", "https://www.cryptalyzer.com/hello");
+formdata.append("enablePKCE", "false");
+formdata.append("scopes", "[\"wallet:user:read\"]");
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: formdata,
+  redirect: 'follow'
+};
+
+fetch("https://api.coinbase.com/oauth/token", requestOptions)
+  .then(response => response.text())
+  .then(result => console.table(result))
+  .catch(error => console.log('error', error));
+
 }, []);
 
   return (
