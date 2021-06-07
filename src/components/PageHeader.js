427d1afde7dfac4b3aaae84b7b4d23fa882fd95b
Christopher Robin Reynolds <christopherrobinreynolds@gmail.com>
2021-06-07 03:33:21 -0400

    Somehow I missed PageHeader in the eslint fixes

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Container, Row, Col } from "reactstrap";
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 
 const header = ({pageTitle}) => {
   return (
